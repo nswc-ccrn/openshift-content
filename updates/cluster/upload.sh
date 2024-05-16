a3803da955f8bb7aefc6bf88311a026b86243c06
Chris Kuperstein <ckuperst@redhat.com>
2024-05-16 14:04:13 -0700

    fixing upload scripts

--- updates/cluster/upload.sh
+++ updates/cluster/upload.sh
@@ -2,7 +2,8 @@
 
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 podman image exists quay.io/nswc-ccrn/oc-mirror:latest || podman load -i $scriptDir/../../images/oc-mirror.tar.gz
-touch .oc-mirror.log
+
+[[ -f .oc-mirror.log ]] || echo '' >.oc-mirror.log
 
 podman run -it --rm --name oc-mirror-upload-cluster-updates \
 	-v /run/user/1000/containers/auth.json:/root/.docker/config.json:ro \
@@ -10,4 +11,4 @@ podman run -it --rm --name oc-mirror-upload-cluster-updates \
 	-v $scriptDir/publish:/publish:z \
 	-v $scriptDir/.oc-mirror.log:/.oc-mirror.log:z \
 	quay.io/nswc-ccrn/oc-mirror:latest \
-	-- "--from=/oc-mirror-workspace docker://$1"
+	--dest-skip-tls --from=/oc-mirror-workspace docker://$1
