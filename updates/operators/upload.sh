052a027d906e251e58377682c8ed7d040c01f475
Chris Kuperstein <ckuperst@redhat.com>
2024-05-22 11:37:30 -0700

    change upload script to use z instead of ro

--- updates/operators/upload.sh
+++ updates/operators/upload.sh
@@ -3,10 +3,8 @@
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 podman image exists quay.io/nswc-ccrn/oc-mirror:latest || podman load -i $scriptDir/../../images/oc-mirror.tar.gz
 
-[[ -f .oc-mirror.log ]] || echo '' >.oc-mirror.log
-
 podman run -it --rm --name oc-mirror-upload-operators \
-	-v /run/user/1000/containers/auth.json:/root/.docker/config.json:ro \
+	-v /run/user/1000/containers/auth.json:/root/.docker/config.json:z \
 	-v $scriptDir/content:/content:z \
 	quay.io/nswc-ccrn/oc-mirror:latest \
 	--dest-skip-tls --from=/content docker://$1
