490bfd9751717adfa5d01f0a256106cf9b89b71b
Chris Kuperstein <ckuperst@redhat.com>
2024-05-22 10:43:40 -0700

    update configurations to hopefully dump tarballs into tarball folders

--- updates/cluster/download.sh
+++ updates/cluster/download.sh
@@ -5,6 +5,7 @@ podman image exists quay.io/nswc-ccrn/oc-mirror:latest || podman pull quay.io/ns
 podman run -d --rm --name oc-mirror-update-cluster \
 	-v $scriptDir/../../pull-secret.txt:/root/.docker/config.json:z \
 	-v $scriptDir/config.yaml:/config.yaml:z \
+	-v $scriptDir/tarballs:/tarballs:z \
 	-v $scriptDir/oc-mirror-workspace:/oc-mirror-workspace:z \
 	-v $scriptDir/publish:/publish:z \
 	quay.io/nswc-ccrn/oc-mirror:latest
--- updates/operators/download.sh
+++ updates/operators/download.sh
@@ -2,6 +2,7 @@
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 podman run -d --rm --name oc-mirror-update-operators \
 	-v $scriptDir/../../pull-secret.txt:/root/.docker/config.json:z \
+	-v $scriptDir/tarballs:/tarballs:z \
 	-v $scriptDir/config.yaml:/config.yaml:z \
 	-v $scriptDir/oc-mirror-workspace:/oc-mirror-workspace:z \
 	-v $scriptDir/publish:/publish:z \
