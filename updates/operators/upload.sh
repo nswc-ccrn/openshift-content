529c6f6bfc0a1633066140c0e9511b23a98f0f0f
Chris Kuperstein <chris@kuperstein.net>
2024-06-10 10:57:23 -0700

    fixing upload script to use /content as workdir

--- updates/operators/upload.sh
+++ updates/operators/upload.sh
@@ -4,6 +4,7 @@ scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 podman image exists quay.io/nswc-ccrn/oc-mirror:latest || podman load -i $scriptDir/../../images/oc-mirror.tar.gz
 
 podman run -it --rm --name oc-mirror-upload-operators \
+	--workdir /content \
 	-v /run/user/1000/containers/auth.json:/root/.docker/config.json:z \
 	-v $scriptDir/content:/content:z \
 	quay.io/nswc-ccrn/oc-mirror:latest \
