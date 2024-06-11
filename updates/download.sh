7706bda3cc6ab6222316cfbc554eedc68026b391
Chris Kuperstein <chris@kuperstein.net>
2024-06-11 12:38:08 -0700

    update download and upload script

--- updates/download.sh
+++ updates/download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-scriptDir=$(realpath $0)
+scriptDir=$(dirname $(readlink -fm $0))
 
-[[ -f $(pwd)/../images/oc-mirror.tar.gz ]] || podman pull quay.io/nswc-ccrn/oc-mirror:latest && podman save --format=oci-archive -o $(pwd)/../images/oc-mirror.tar.gz quay.io/nswc-ccrn/oc-mirror:latest
+[[ -f $scriptDir/../images/oc-mirror.tar.gz ]] || podman pull quay.io/nswc-ccrn/oc-mirror:latest && podman save --format=oci-archive -o $scriptDir/../images/oc-mirror.tar.gz quay.io/nswc-ccrn/oc-mirror:latest
 
-podman image exists quay.io/nswc-ccrn/oc-mirror:latest || podman load -i $(pwd)/../images/oc-mirror.tar.gz
+podman image exists quay.io/nswc-ccrn/oc-mirror:latest || podman load -i $scriptDir/../images/oc-mirror.tar.gz
 
-podman run -d --rm --name oc-mirror-update-cluster \
+podman run -d --name oc-mirror-update-cluster \
 	--workdir /content \
-	-v $scriptDir/../../pull-secret.txt:/root/.docker/config.json:z \
-	-v ./config.yaml:/config.yaml:z \
-	-v ./content:/content:z \
+	-v $scriptDir/../pull-secret.txt:/root/.docker/config.json:z \
+	-v $scriptDir/config.yaml:/config.yaml:z \
+	-v $scriptDir/content:/content:z \
 	quay.io/nswc-ccrn/oc-mirror:latest \
 	--config=/config.yaml file://.
